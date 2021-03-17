e549b4a5214761ff96ede39dbb1833fb299c0fe9
ingewerner <ingewetner1@gmail.com>
2021-03-17 16:41:01 +0100

    add merging

--- src/data-preparation/merging.R
+++ src/data-preparation/merging.R
@@ -1,7 +1,7 @@
-BLM2020 <- read.csv('BLM2020_Dataset.csv', sep = '\t', na.strings=c("", "NA"))
-BLM2021 <- read.csv('BLM2021_Dataset.csv', sep = '\t', na.strings=c("", "NA"))
+BLM2020_filterd <- read.csv('../../gen/data-preparation/temp/tempfile1.csv', sep = '\t', na.strings=c("", "NA"))
+BLM2021_filtered <- read.csv('../../gen/data-preparation/temp/tempfile2.csv', sep = '\t', na.strings=c("", "NA"))
 
 
 # make one file (first rows are 2020)
-BLM_merged <- rbind(BLM2020, BLM2021)
+BLM_merged <- rbind(BLM2020_filterd, BLM2021_filtered)
 
